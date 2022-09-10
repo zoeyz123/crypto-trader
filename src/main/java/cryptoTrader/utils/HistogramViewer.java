2fd533cc56857d8194969c0808743b689d3cf7c4
Zoey Zheng <73442393+zoeyz123@users.noreply.github.com>
2022-09-09 21:52:16 -0400

    Update HistogramViewer.java

--- src/main/java/cryptoTrader/utils/HistogramViewer.java
+++ src/main/java/cryptoTrader/utils/HistogramViewer.java
@@ -1,6 +1,5 @@
 /*
  * Class Description: This class creates the histogram representation of the user's trade
- * Authors: Jessica Ou, Brielle Nguyen, Mylan Nguyen, Zoey Zheng
  */
 
 package cryptoTrader.utils;
